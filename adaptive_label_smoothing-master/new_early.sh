cfa60920ed58a875eae990d0d8e79a557392f2a8
Wang Ru <wangru1994305@gmail.com>
2019-09-25 12:12:31 +0800

    revise

--- adaptive_label_smoothing-master/new_early.sh
+++ adaptive_label_smoothing-master/new_early.sh
@@ -1,6 +1,7 @@
 for nr in 0.2 0.5 0.65 0.8 0.85
+  do
   for seed_num in 1 2 3
   do
-    python loss_el.py --batch-size 128 --dataset mnist --noise_type symmetric --noise_rate ${nr} --n_epoch 50 --eps 9.99 --seed ${seed_num}
+    python vali_el.py --batch-size 128 --dataset mnist --noise_type symmetric --noise_rate ${nr} --n_epoch 50 --eps 9.99 --seed ${seed_num}
   done
 done
